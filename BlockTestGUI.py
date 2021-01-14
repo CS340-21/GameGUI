d5e40207f672ebe3acf8e5251945183f016b194e
Heather Haynie <hhaynie@vols.utk.edu>
2021-01-14 12:44:22 -0500

    Dynamically adjust to row and col sizes. Bugs when get to sizes like 10x10.

--- BlockTestGUI.py
+++ BlockTestGUI.py
@@ -6,11 +6,17 @@ x, y = auto.size()
 print(x)
 print(y)
 
+# Create a dictionary of all possible row and column values to loop through all possible options
+
 MAX_ROWS = 2
-MAX_COL = 3
+MAX_COL = 2
+
+print(x/MAX_COL)
+print(y/MAX_ROWS)
+
 board = [[1 for j in range(MAX_COL)] for i in range(MAX_ROWS)]
-layout = [[sg.Button(size=(20, 10), button_color=('red', 'green'), key=(i,j)) for j in range(MAX_COL)] for i in range(MAX_ROWS)]
-window = sg.Window(title="6q-Interface", layout=layout)  # margins=(1200 ,1000)).read()
+layout = [[sg.Button(size=(int(x/8/MAX_COL), int(y/17/MAX_ROWS)), button_color=('red', 'green'), key=(i,j)) for j in range(MAX_COL)] for i in range(MAX_ROWS)]
+window = sg.Window(title="6q-Interface", size=(x, y), layout=layout)  # margins=(1200 ,1000)).read()
 
 # print(window)
 while True:
