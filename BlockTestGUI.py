36c73cfbd9bfd61fb912f3a5f38e2c8c3ca74365
Heather Haynie <hhaynie@vols.utk.edu>
2021-01-15 15:28:17 -0500

    Logs information from GUI to csv file.

--- BlockTestGUI.py
+++ BlockTestGUI.py
@@ -1,6 +1,8 @@
 import pyautogui as auto
 import PySimpleGUI as sg
+import pandas as pd
 import time
+import csv
 
 x, y = auto.size()
 print(x)
@@ -14,6 +16,8 @@ MAX_COL = 2
 print(x/MAX_COL)
 print(y/MAX_ROWS)
 
+df = pd.DataFrame()
+
 board = [[1 for j in range(MAX_COL)] for i in range(MAX_ROWS)]
 layout = [[sg.Button(size=(int(x/8/MAX_COL), int(y/17/MAX_ROWS)), button_color=('red', 'green'), key=(i,j)) for j in range(MAX_COL)] for i in range(MAX_ROWS)]
 window = sg.Window(title="6q-Interface", size=(x, y), layout=layout)  # margins=(1200 ,1000)).read()
@@ -25,10 +29,19 @@ while True:
         break
 
     else:
-        window[event].update(button_color=("green", "red"))
+        window[event].update(button_color=('green', 'red'))
         window = window.refresh()
         time.sleep(.5)
-        window[event].update(button_color=("red", "green"))
+        window[event].update(button_color=('red', 'green'))
         window = window.refresh()
 
+        df = df.append([[MAX_ROWS, MAX_COL, event, None]], ignore_index=True)
+
+headers = ['Max Rows', 'Max Cols', 'Block', 'Image']
+df.columns = headers
+
+file = 'block_test_results.csv'
+df.to_csv(file, na_rep='-', header=True, index=False)
+
+print(df)
 window.close()
